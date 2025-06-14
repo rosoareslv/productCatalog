a5133a4de219e68a218424514d572047b3f39672
Rodrigo Soares <rodrigosoaresilva@gmail.com>
2025-06-14 16:03:00 -0300

    adding basic api documentation

--- api/routes/category.js
+++ api/routes/category.js
@@ -1,20 +1,83 @@
 const express = require("express")
 const router = express.Router()
 
+/** 
+ * @openapi
+ * /category/save:
+ *   post:
+ *     summary: Salva categoria no banco
+ *     tags: [Category]
+ *     responses:
+ *       200:
+ *         description: Categoria cadastrado com sucesso.
+ *       500:
+ *          description: Erro interno ao cadastrar categoria
+ */
 router.post('/save', () => {
 
 })
 
+/** 
+ * @openapi
+ * /category/update:
+ *   patch:
+ *     summary: Atualiza registro de categoria no banco
+ *     tags: [Category]
+ *     responses:
+ *       200:
+ *         description: Categoria atualizado com sucesso.
+ *       500:
+ *          description: Erro interno ao atualizar categoria
+ */
 router.patch("/update", (req, res) => {
     console.log(req.client)
 
 })
 
+/** 
+ * @openapi
+ * /category/:id:
+ *   get:
+ *     summary: Retorna informações específicas da categoria
+ *     tags: [Category]
+ *     responses:
+ *       200:
+ *         description: Categoria retornada com sucesso.
+ *       500:
+ *          description: Erro interno ao retornar categoria
+ */
+router.get("/:id", (req, res) => {
+
+})
+
+/** 
+ * @openapi
+ * /category/:id:
+ *   delete:
+ *     summary: Deletar categoria do banco de dados
+ *     tags: [Category]
+ *     responses:
+ *       200:
+ *         description: Categoria excluída com sucesso.
+ *       500:
+ *          description: Erro interno ao excluir categoria
+ */
 router.delete("/:id", (req, res) => {
 
 })
 
-
+/** 
+ * @openapi
+ * /category/list:
+ *   get:
+ *     summary: Retornar todas as categorias cadastradas
+ *     tags: [Category]
+ *     responses:
+ *       200:
+ *         description: Categorias retornadas com sucesso.
+ *       500:
+ *          description: Erro interno ao retornar categorias.
+ */
 router.get("/list", () => {
 
 })
--- api/routes/product.js
+++ api/routes/product.js
@@ -1,28 +1,104 @@
 const express = require("express")
 const router = express.Router()
 
+/** 
+ * @openapi
+ * /product/save:
+ *   post:
+ *     summary: Salva produto no banco
+ *     tags: [Product]
+ *     responses:
+ *       200:
+ *         description: Produto cadastrado com sucesso.
+ *       500:
+ *          description: Erro interno ao cadastrar produto
+ */
 router.post("/save", (req, res) => {
     console.log(req.client)
 
 })
 
+/** 
+ * @openapi
+ * /product/update:
+ *   patch:
+ *     summary: Atualiza produto no banco
+ *     tags: [Product]
+ *     responses:
+ *       200:
+ *         description: Produto atualizado com sucesso.
+ *       500:
+ *          description: Erro interno ao atualixar produto
+ */
 router.patch("/update", (req, res) => {
     console.log(req.client)
 
 })
 
+/** 
+ * @openapi
+ * /product/:id:
+ *   get:
+ *     summary: Retorna informações específicas do produto
+ *     tags: [Product]
+ *     responses:
+ *       200:
+ *         description: Produto retornado com sucesso.
+ *       500:
+ *          description: Erro interno ao retornar produto.
+ */
+//Só pode ser associado com uma categoria por vez
+router.get("/:id", (req, res) => {
+
+})
+
+/** 
+ * @openapi
+ * /product/:id:
+ *   delete:
+ *     summary: Deleta produto no banco de dados
+ *     tags: [Product]
+ *     responses:
+ *       200:
+ *         description: Produto excluído com sucesso.
+ *       500:
+ *          description: Erro interno ao excluir produto
+ */
 //Só pode ser associado com uma categoria por vez
 router.delete("/:id", (req, res) => {
 
 })
 
+/** 
+ * @openapi
+ * /product/list:
+ *   get:
+ *     summary: Retorna todos os produtos cadastrados
+ *     tags: [Product]
+ *     responses:
+ *       200:
+ *         description: Produtos retornados com sucesso.
+ *       500:
+ *          description: Erro interno ao retornar produtos.
+ */
 router.get("/list", (req, res) => {
     console.log(req.client)
 
 })
 
 
-//associar produto com categoria
+/** 
+ * @openapi
+ * /product/associate:
+ *   patch:
+ *     summary: Associa um produto a uma categoria
+ *     tags: [Product]
+ *     responses:
+ *       200:
+ *         description: Produto associado a categoria.
+ *       500:
+ *         description: Erro interno ao associar produto com categoria.
+ */
 router.patch("/associate", (req, res) => {
 
 })
