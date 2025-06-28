1be9ec25e6b011f5c6b6cb33f6ae6b8b255afc2c
Rodrigo Soares <soares@Rodrigos-MacBook-Air.local>
2025-06-27 22:48:27 -0300

    adding token file with create token implemented

--- api/helper/token.js
+++ api/helper/token.js
@@ -3,9 +3,8 @@ const fs = require("fs")
 
 const privateKey = fs.readFileSync("./keys/id_rsa").toString()
 
-function createToken() {
-    //TODO gerar token com expiracao definida
-    return jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' });
+function createToken(username) {
+    return jwt.sign({ username: username }, privateKey, { algorithm: 'RS256', expiresIn: '24h' });
 }
 
 function checkToken() {
