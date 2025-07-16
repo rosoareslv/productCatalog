e3e3aab24ee56ba936e3469c92547e33c7fafafb
Rodrigo Soares <rodrigosoaresilva@gmail.com>
2025-07-16 17:34:04 -0300

    adding new validators to endpoints

--- api/helper/validators.js
+++ api/helper/validators.js
@@ -1,14 +1,45 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
-const loginSchema = Joi.object({
-    username: Joi.string().alphanum().max(20).required(),
-    password: Joi.string().alphanum().max(20).required()
-})
+const loginUser = Joi.object({
+  username: Joi.string().alphanum().max(20).required(),
+  password: Joi.string().alphanum().max(20).required(),
+});
 
-const registerSchema = Joi.object({
-    name: Joi.string().alphanum().max(20).required(),
-    username: Joi.string().alphanum().max(20).required(),
-    password: Joi.string().alphanum().required()
-})
+const registerUser = Joi.object({
+  name: Joi.string().alphanum().max(20).required(),
+  username: Joi.string().alphanum().max(20).required(),
+  password: Joi.string().alphanum().required(),
+});
 
-module.exports = { loginSchema, registerSchema }
+const registerProduct = Joi.object({
+  title: Joi.string().max(20).required(),
+  description: Joi.string().max(50).required(),
+  price: Joi.number().min(0).required(),
+  category: Joi.string().max(20),
+});
+
+const updateProduct = Joi.object({
+  title: Joi.string().max(20),
+  description: Joi.string().max(50),
+  price: Joi.number().min(0),
+  category: Joi.string().max(20),
+});
+
+const registerCategory = Joi.object({
+  title: Joi.string().max(20).required(),
+  description: Joi.string().max(50).required(),
+});
+
+const updateCategory = Joi.object({
+  title: Joi.string().max(20),
+  description: Joi.string().max(50),
+});
+
+module.exports = {
+  loginUser,
+  registerUser,
+  registerProduct,
+  updateProduct,
+  registerCategory,
+  updateCategory,
+};
