04d3e607d28b90d2f055346192290b7ce6eb18f9
Rodrigo Soares <rodrigosoaresilva@gmail.com>
2025-07-17 00:43:10 -0300

    Adding category endpoints to swagger validation

--- api/app.js
+++ api/app.js
@@ -17,12 +17,14 @@ const productSchemas = JSON.parse(
   fs.readFileSync("./docs/schemas/product.json")
 );
 const authSchemas = JSON.parse(fs.readFileSync("./docs/schemas/auth.json"));
+const categorySchemas = JSON.parse(fs.readFileSync("./docs/schemas/category.json"))
 
 const authPaths = JSON.parse(fs.readFileSync("./docs/paths/auth.json"));
 const productPaths = JSON.parse(fs.readFileSync("./docs/paths/product.json"));
+const categorypaths = JSON.parse(fs.readFileSync("./docs/paths/category.json"))
 
-openapi.paths = paths = { ...authPaths, ...productPaths };
-openapi.components.schemas = { ...productSchemas, ...authSchemas };
+openapi.paths = paths = { ...authPaths, ...productPaths, ...categorypaths };
+openapi.components.schemas = { ...productSchemas, ...authSchemas, ...categorySchemas };
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapi));
 
