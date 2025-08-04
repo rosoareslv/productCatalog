ede6ceeeea55bd259a3b06182616b865dd1a9b13
Rodrigo Soares <rodrigosoaresilva@gmail.com>
2025-08-04 00:15:01 -0300

    redis file configuration to implement refreshTokens whitelist

---
+++ api/helper/redis.js
@@ -0,0 +1,27 @@
+const { createClient } = require("redis");
+
+const redisClient = createClient({
+  url: "redis://whitelist:6379",
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+redisClient.on("end", () => {
+  console.log("Redis disconnected");
+});
+
+async function connect() {
+  try {
+    await redisClient.connect();
+  } catch (error) {
+    throw error;
+  }
+}
+
+connect();
+
+module.exports = {
+  redisClient,
+};
