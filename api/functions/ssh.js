f60ff2e783e0793d80f1b620343d12339a388db4
Rodrigo Soares <rodrigosoaresilva@gmail.com>
2025-08-06 21:48:13 -0300

    adding ssh function to create pair key when initialize app

---
+++ api/functions/ssh.js
@@ -0,0 +1,21 @@
+const { exec } = require("child_process");
+const fs = require("fs")
+const util = require("util");
+
+const execAsync = util.promisify(exec);
+
+async function createSSHKeys() {
+  try {
+    const { stdout, stderr } = await execAsync(
+      "echo 'yes' | ssh-keygen -t rsa -b 4096 -m PEM -f /keys/id_rsa -N ''"
+    )
+    if (stdout) console.log("SSH Keys OK");
+    if (stderr) console.log("SSH Key Error:", stderr);
+  } catch (error) {
+    throw error;
+  }
+}
+
+process.env.PRIVATE_KEY = fs.readFileSync("/keys/id_rsa").toString();
+
+createSSHKeys()
