861e098b2b54b1901b2edf8cf83e31119131ad86
Rodrigo Soares <rodrigosoaresilva@gmail.com>
2025-08-26 20:35:10 -0300

    Expanding characters limit in fields

--- api/functions/validators.js
+++ api/functions/validators.js
@@ -6,31 +6,31 @@ export const loginUserValidator = Joi.object({
 });
 
 export const registerUserValidator = Joi.object({
-  name: Joi.string().max(20).required(),
+  name: Joi.string().max(50).required(),
   username: Joi.string().alphanum().max(20).required(),
   password: Joi.string().alphanum().required(),
 });
 
 export const registerProductValidator = Joi.object({
-  title: Joi.string().max(20).required(),
-  description: Joi.string().max(50).required(),
+  title: Joi.string().max(50).required(),
+  description: Joi.string().max(100).required(),
   price: Joi.number().min(0).required(),
   category: Joi.string().alphanum().max(20),
 });
 
 export const updateProductValidator = Joi.object({
-  title: Joi.string().max(20),
-  description: Joi.string().max(50),
+  title: Joi.string().max(50),
+  description: Joi.string().max(100),
   price: Joi.number().min(0),
-  category: Joi.string().max(20),
+  category: Joi.string().max(50),
 });
 
 export const registerCategoryValidator = Joi.object({
-  title: Joi.string().alphanum().max(20).required(),
-  description: Joi.string().max(50).required(),
+  title: Joi.string().alphanum().max(50).required(),
+  description: Joi.string().max(100).required(),
 });
 
 export const updateCategoryValidator = Joi.object({
-  title: Joi.string().alphanum().max(20),
-  description: Joi.string().max(50),
+  title: Joi.string().alphanum().max(50),
+  description: Joi.string().max(100),
 });
